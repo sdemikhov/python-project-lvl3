762bed72be737bf7ad7ea8b6259c7f4367a99f79
python <python@debian>
2020-05-25 03:48:14 -0500

    start step2: download page with resources

--- page_load/page_load.py
+++ page_load/page_load.py
@@ -1,40 +1,82 @@
-import requests
 import re
 from pathlib import Path
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urljoin
 
-MESSAGE_TEMPATE = "Page wasn't downloaded! Responce code {}"
+
+EXTENSION = '.html'
+SUFFIX = '_files'
 
 
 def page_loader(target_url, destination=None):
-    request = requests.get(target_url)
+    response = send_request(target_url)
 
-    if request.ok:
-        filename = make_filename(target_url)
+    page_filename = make_filename(target_url)
+    if destination is None:
+        destination_path = Path.cwd()
+    else:
+        destination_path = Path(destination)
+    soup = BeautifulSoup(response.text)
+    tags_with_resources = soup.find_all(certain_tag_has_certain_src)
+    if tags_with_resources:
+        resources = []
+        resources_directory = Path(page_filename + SUFFIX)
+        for tag in tags_with_resources:
+            resource_filename = make_filename(tag['src'])
+            resource_url = urljoin(target_url, tag['src'])
+            resources.append((resource_filename, resource_url))
+            new_src = resources_directory / resource_filename
+            tag['src'] = new_src
+    if not destination_path.exists():
+        destination_path.mkdir()
+    with open(destination_path / (page_filename + EXTENSION), 'w') as f:
+        f.write(soup.prettify())
+    if resources:
+        if not (destination_path / resources_directory).exists():
+            (destination_path / resources_directory).mkdir()
+        for resource_filename, resource_url in resources:
+            resource_response = send_request(resource_url)
+            with open(destination_path / resources_directory / resource_filename, 'w') as f:
+                f.write(resource_response.text)
 
-        if destination is None:
-            path = Path.cwd()
-        else:
-            path = Path(destination)
 
-        with open(path / filename, 'w') as f:
-            f.write(request.text)
+MESSAGE_TEMPATE = "File '{url}' wasn't downloaded! Response code '{code}'"
+
+
+def send_request(url_):
+    response = requests.get(url_)
+    if response.ok:
+        return response
     else:
         raise ValueError(
-            MESSAGE_TEMPATE.format(request.status_code)
+            MESSAGE_TEMPATE.format(url=url_, code=response.status_code)
         )
 
-
-SCHEME = r'^.*//'
+SCHEME = r'^.*://|^/'
 NOT_LETTERS_OR_DIGITS = r'[^a-zA-Z0-9]'
 SEPARATOR = '-'
-EXTANSION = '.html'
 
 
 def make_filename(target_url):
     url_without_scheme = re.sub(SCHEME, '', target_url)
-    filename_without_extansion = re.sub(
+    return re.sub(
         NOT_LETTERS_OR_DIGITS,
         SEPARATOR,
         url_without_scheme,
     )
-    return filename_without_extansion + EXTANSION
+
+
+IMPORTANT_TAGS = [
+    'link',
+    'script',
+    'img',
+]
+LOCAL_RESOURCES = r'^/'
+
+def certain_tag_has_certain_src(tag):
+    return (
+        tag.name in IMPORTANT_TAGS and
+        tag.has_attr('src') and
+        re.search(LOCAL_RESOURCES, tag['src'])
+    )
