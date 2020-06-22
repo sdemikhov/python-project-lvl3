c9f862409875914e047a504b644ccfca0e389585
Sergey Demikhov <demikhov.sergey@gmail.com>
2020-06-22 09:35:37 -0500

    fix exit error codes and debug messages

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,28 +1,37 @@
 import sys
 from logging import getLogger
+import traceback
 
-from page_loader.cli import parser
-from page_loader.core import download_page, PageLoaderError
-from page_loader import logging
+from page_loader import cli, logging, core
 
 
 def main():
-    arguments = parser.parse_args()
+    arguments = cli.parser.parse_args()
     logging.setup(arguments.log_level)
     logger = getLogger()
     logger.debug(
         'User passed following arguments: {}'.format(arguments)
     )
     try:
-        download_page(
+        core.download_page(
             arguments.target_url,
             destination=arguments.destination,
         )
-    except PageLoaderError as e:
+    except core.PageLoaderError as e:
         logger.error(str(e))
         if e.__cause__:
-            logger.debug(e.__cause__)
+            logger.debug(traceback.format_exc())
+
+        if str(e).startswith(core.REQUEST_ERROR_PREFIX):
+            sys.exit(2)
+        elif str(e).startswith(core.DIRECTORY_ERROR_PREFIX):
+            sys.exit(3)
+        elif str(e).startswith(core.WRITE_ERROR_PREFIX):
+            sys.exit(4)
         sys.exit(1)
+
+    else:
+        sys.exit(0)
 
 
 if __name__ == "__main__":
