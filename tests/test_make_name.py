4caa057c3e95695ab8842e5774fcccb782a211ef
Sergey Demikhov <demikhov.sergey@gmail.com>
2020-07-06 01:55:04 -0500

    update tests

--- tests/test_make_name.py
+++ tests/test_make_name.py
@@ -14,13 +14,13 @@ def test_make_short_name():
         "name wrong numerated"
     )
 
-    long_name = 'a' * 300
+    long_name = 'a' * (core.MAX_FILENAME_LENGTH + 1)
     expected_long = long_name[:core.MAX_FILENAME_LENGTH]
     assert core.make_short_name(long_name, '') == expected_long, (
         "name wrong cutted"
     )
 
-    long_name2 = 'b' * 300
+    long_name2 = 'b' * core.MAX_FILENAME_LENGTH
     expected_long2 = (
         long_name2[:(core.MAX_FILENAME_LENGTH) - len(extension)] + extension
     )
@@ -28,7 +28,7 @@ def test_make_short_name():
         "name wrong cutted with extension"
     )
 
-    long_extesion = 'c' * 300
+    long_extesion = 'c' * core.MAX_FILENAME_LENGTH
     assert core.make_short_name(name, long_extesion) == '', (
         'must return zero-string when long extension'
     )
@@ -37,11 +37,52 @@ def test_make_short_name():
     )
 
     short_name = 'd'
-    long_extesion3 = 'e' * 254
-    expected_long3 = short_name + long_extesion3
-    assert core.make_short_name(short_name, long_extesion3) == expected_long3, (
+    extesion3 = 'e' * (core.MAX_FILENAME_LENGTH - 1)
+    expected3 = short_name + extesion3
+    assert core.make_short_name(short_name, extesion3) == expected3, (
         "name don't glued"
     )
-    assert core.make_short_name(short_name, long_extesion3) == '', (
+    assert core.make_short_name(short_name, extesion3) == '', (
         "must return zero-string, can't cut name for numerate"
+    )
+
+
+def test_make_name_from_url():
+    url_http = 'http://www.example.com'
+    name_from_http = 'www-example-com'
+    assert core.make_name_from_url(url_http) == name_from_http, (
+        'must clear schema http://'
+    )
+
+    url_https = 'https://www.some-site.com'
+    name_from_https = 'www-some-site-com'
+    assert core.make_name_from_url(url_https) == name_from_https, (
+        'must clear schema https://'
+    )
+
+    resource_url = 'https://www.some-site.com/images/logo.svg'
+    resource_name = 'www-some-site-com-images-logo.svg'
+    assert core.make_name_from_url(
+        resource_url,
+        search_extension=True
+    ) == resource_name, ('must contain file extension')
+
+    resource_url_long_ext = ('https://www.example.com/static/file.' + 
+                             'a' * core.MAX_FILENAME_LENGTH)
+    resource_name_long_ext = (
+        'www-example-com-static-file-' + 'a' * core.MAX_FILENAME_LENGTH
+    )[:core.MAX_FILENAME_LENGTH]
+    assert core.make_name_from_url(
+        resource_url_long_ext,
+        search_extension=True
+    ) == resource_name_long_ext, ('if long extension, skip search extension')
+
+    url_encoded = (
+        'https://ru.example.org/%D0%97%D0%B0%D0%B3%D0' + 
+        '%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1' + 
+        '%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0'
+    )
+    ru_name = 'ru-example-org-Заглавная-страница'
+    assert core.make_name_from_url(url_encoded) == ru_name, (
+        'must unquote chatacters'
     )
